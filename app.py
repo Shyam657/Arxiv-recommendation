4a2f7335092294b53c765c385d4da900be5f22ce
Shyam <103296209+Shyam657@users.noreply.github.com>
2022-11-10 09:33:39 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,73 +1,43 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "dda67103",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "import pickle\n",
-    "import streamlit as st\n",
-    "import requests\n",
-    "import numpy as np"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "b6a5154f",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "movies = pickle.load(open('model/movie_list.pkl','rb'))\n",
-    "similarity = pickle.load(open('model/similarity.pkl','rb'))"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "df54b735",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "embeddings=np.load(r\"C:\\Users\\shyam\\Downloads\\arxiv\\arxivtitle_embeddings_10-11-2022.npy\")\n",
-    "papers=pickle.load(open(r\"C:\\Users\\shyam\\Downloads\\arxiv\\papers.pkl\",'rb'))"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "ee66b4a2",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "import torch\n",
-    "from sentence_transformers import SentenceTransformer, util\n",
-    "\n",
-    "def recommend(query):\n",
-    "    #Compute cosine-similarities with all embeddings \n",
-    "    query_embedd = model.encode(query)\n",
-    "    cosine_scores = util.pytorch_cos_sim(query_embedd, embeddings)\n",
-    "    top5_matches = torch.argsort(cosine_scores, dim=-1, descending=True).tolist()[0][1:6]\n",
-    "    return top5_matches\n",
-    "\n",
-    "id = 'quantum'\n",
-    "#query_show_des = papers.loc[netflix_data['show_id'] == id]['description'].to_list()[0]\n",
-    "recommendded_results = recommend(id)\n",
-    "\n",
-    "for index in recommendded_results:\n",
-    "    st.text(papers.iloc[index,:])"
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3 (ipykernel)",
-   "language": "python",
-   "name": "python3"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+import pickle
+import streamlit as st
+import requests
+import numpy as np
+import torch
+from sentence_transformers import SentenceTransformer, util
+
+
+
+
+
+
+
+embeddings=np.load(r"C:\Users\shyam\Downloads\arxiv\arxivtitle_embeddings_10-11-2022.npy")
+papers=pickle.load(open('papers.pkl','rb'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def recommend(query):
+    #Compute cosine-similarities with all embeddings
+    query_embedd = model.encode(query)
+    cosine_scores = util.pytorch_cos_sim(query_embedd, embeddings)
+    top5_matches = torch.argsort(cosine_scores, dim=-1, descending=True).tolist()[0][1:6]
+    return top5_matches
+
+id = 'quantum'
+#query_show_des = papers.loc[netflix_data['show_id'] == id]['description'].to_list()[0]
+recommendded_results = recommend(id)
+
+for index in recommendded_results:
+    st.text(papers.iloc[index,:])

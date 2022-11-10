c0e3ff6f70cc3d8430fa6c0a40cb12aaeac9366e
Shyam <103296209+Shyam657@users.noreply.github.com>
2022-11-10 11:27:01 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -59,7 +59,7 @@ with header_container:
 	# different levels of text you can include in your app
 	st.title("Arxiv Research Paper Recommender")
 	#st.header("Welcome!")
-    st.image('arxiv.png')
+        st.image('arxiv.png')
 	#st.subheader("This is a great app")
 	st.write(
         "Searching For recommendation of simmilar research Paper ?",

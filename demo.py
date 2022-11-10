c05305e4317195f274e007659c05943a2a937d83
Shyam <103296209+Shyam657@users.noreply.github.com>
2022-11-10 11:21:35 +0530

    Create demo.py

---
+++ demo.py
@@ -0,0 +1,83 @@
+import pickle
+import streamlit as st
+import requests
+import numpy as np
+import torch
+from sentence_transformers import SentenceTransformer, util
+
+model = SentenceTransformer('sentence-transformers/bert-base-nli-mean-tokens')
+
+embeddings=np.load("arxivtitle_embeddings_10-11-2022.npy")
+papers=pickle.load(open('papers.pkl','rb'))
+
+def recommend(query):
+    #Compute cosine-similarities with all embeddings
+    query_embedd = model.encode(query)
+    cosine_scores = util.pytorch_cos_sim(query_embedd, embeddings)
+    top5_matches = torch.argsort(cosine_scores, dim=-1, descending=True).tolist()[0][1:6]
+    return top5_matches
+
+
+st.set_page_config(layout="wide")
+
+
+
+
+
+st.markdown(
+        f"""
+<style>
+    .reportview-container .main .block-container{{
+        max-width: 70%;
+        padding-top: 5rem;
+        padding-right: 5rem;
+        padding-left: 5rem;
+        padding-bottom: 5rem;
+    }}
+    img{{
+    	max-width:40%;
+    	margin-bottom:40px;
+    }}
+</style>
+""",
+        unsafe_allow_html=True,
+    )
+#################
+
+header_container = st.beta_container()
+
+
+
+
+
+
+with header_container:
+
+	# for example a logo or a image that looks like a website header
+	
+
+	# different levels of text you can include in your app
+	st.title("Arxiv Research Paper Recommender")
+	#st.header("Welcome!")
+    st.image('arxiv.png')
+	#st.subheader("This is a great app")
+	st.write(
+        "Searching For recommendation of simmilar research Paper ?",
+        "Your search ends here !!! \U0001F642")
+
+
+
+selected_keyword = st.text_input(
+    "Type Keyword for Related Paper .",'Quantum'
+)
+
+
+recommendded_results = recommend(selected_keyword)
+
+if st.button('Show Recommendation'):
+    
+    for index in recommendded_results:
+        
+        #st.text(papers.iloc[index,:])
+        st.text(papers.iloc[index].iloc[0])
+    
